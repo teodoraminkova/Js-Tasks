4f3088a5911d91e48f6d8fee340edb9ac7c1c969
Teodora Minkova <50526766+teodoraminkova@users.noreply.github.com>
2019-09-24 18:17:51 +0300

    Split tasks to different files.

---
+++ js-objects2.js
@@ -0,0 +1,53 @@
+/* SECOND TASK
+    If the student has been graduated his taxForUni will be 0 and graduated will be true but finalScore will be from 2 to 6.
+    If the student has not been graduated it will have finalScore equal to 0, graduated = false and taxForUni will be a digit which is bigger than 0. 
+    
+    REQUIREMENTS: Please show in console: 
+    1. How many students had been graduated? 
+    2. Average taxForUni 
+    3. Names to students that have more than 4 as finalScore. 
+    4. Name and Uni as ‘Ivan is/was studing in UniName’ 
+    5. UniName and Average finalScore 
+*/
+let students = [
+    { 
+    "studentName" : "Ivan Brown",
+    "age": 27,
+    "university": "Leeds",
+    "taxForUni": 0,
+    "graduated": true,
+    "finalScore": 5
+    },
+    { 
+        "studentNamе" : "John Kenedy",
+        "age": 19,
+        "university": "Leeds",
+        "taxForUni": 1299,
+        "graduated": false,
+        "finalScore": 0 
+    } ,
+    { 
+        "studentNamе" : "Peter Duck",
+        "age": 22,
+        "university": "Manchester",
+        "taxForUni": 3000,
+        "graduated": false,
+        "finalScore": 0 
+    } ,
+    { 
+        "studentNamе" : "Rick Bud",
+        "age": 29,
+        "university": "London",
+        "taxForUni": 0,
+        "graduated": true,
+        "finalScore": 4
+    } ,
+    { 
+        "studentNamе" : "Erica John",
+        "age": 23,
+        "university": "Leeds",
+        "taxForUni": 800,
+        "graduated": false,
+        "finalScore": 0
+    }
+]
