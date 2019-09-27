a0dae0cc4783fd1d9e3b9212b33c738a5dce9e8d
Teodora Minkova <50526766+teodoraminkova@users.noreply.github.com>
2019-09-27 14:33:47 +0300

    Second task is finally finished.

--- js-objects2.js
+++ js-objects2.js
@@ -11,59 +11,64 @@
 */
 
 let students = [
-    { "studentName" : "Ivan Brown", "age": 27, "university": "Leeds", "taxForUni": 0, "graduated": true, "finalScore": 5 },
-    { "studentName" : "John Kenedy", "age": 19, "university": "Leeds", "taxForUni": 1299, "graduated": false, "finalScore": 0 },
-    { "studentName" : "Peter Duck", "age": 22, "university": "Manchester", "taxForUni": 3000, "graduated": false, "finalScore": 0 },
-    { "studentName" : "Rick Bud", "age": 29, "university": "London", "taxForUni": 0, "graduated": true, "finalScore": 4 },
-    { "studentName" : "Erica John", "age": 23, "university": "Leeds", "taxForUni": 800, "graduated": false, "finalScore": 0 } ];
+    { "studentName": "Ivan Brown", "age": 27, "university": "Leeds", "taxForUni": 0, "graduated": true, "finalScore": 5 },
+    { "studentName": "John Kenedy", "age": 19, "university": "Leeds", "taxForUni": 0, "graduated": true, "finalScore": 2 },
+    { "studentName": "Peter Duck", "age": 22, "university": "Manchester", "taxForUni": 3000, "graduated": false, "finalScore": 0 },
+    { "studentName": "Rick Bud", "age": 29, "university": "London", "taxForUni": 0, "graduated": true, "finalScore": 4 },
+    { "studentName": "Erica John", "age": 23, "university": "Leeds", "taxForUni": 800, "graduated": false, "finalScore": 0 }];
 
-function solve(){    
-    // Task 1.
-
-    // console.log(students.filter(x=>x.graduated).length)
+function solve() {
     console.log(graduatedStudents(students));
+    console.log('----------------------');
     console.log(average(students));
+    console.log('----------------------');
     console.log(studentsFinalScore(students));
-    console.log(studentsAndUnis(students));
+    console.log('----------------------');
+    studentsAndUnis(students);
+    console.log('----------------------');
     avgUniScore(students);
 }
 
-function graduatedStudents(students){
+// Task 1.
+// console.log(students.filter(x=>x.graduated).length)
+function graduatedStudents(students) {
     var counter = 0;
-    for(let i = 0; i < students.length; i++){
-        if(students[i].graduated){ 
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].graduated) {
             counter++;
         }
     }
-    return counter;
+    return counter + ' student/s graduated!';
 }
 
+
 //Task 2.
-function average(students){
+function average(students) {
     let sum = 0;
-    for(let t = 0; t < students.length; t++){
-        if(students[t].taxForUni != 0){        
-        sum += students[t].taxForUni;
+    for (let t = 0; t < students.length; t++) {
+        if (students[t].taxForUni != 0) {
+            sum += students[t].taxForUni;
         }
-    }   
+    }
     let avg = (sum / students.length);
-    return avg.toFixed(2);
+    return 'Average tax for uni is: ' + avg.toFixed(2) + '.';
 }
 
 //Task 3.
 function studentsFinalScore(students) {
-    for(let f = 0; f < students.length; f++){
-        if(students[f].finalScore > 4){
-            return students[f].studentName;
+    for (let f = 0; f < students.length; f++) {
+        if (students[f].finalScore > 4) {
+            return 'Final score more than 4: ' + students[f].studentName;
         }
     }
 }
 
+
 //Task 4.
-function studentsAndUnis(students){
-    for(let s = 0; s < students.length; s++){
-        if(students[s].graduated){
-        console.log(students[s].studentName + ' was studying at in ' + students[s].university + ' university.');
+function studentsAndUnis(students) {
+    for (let s = 0; s < students.length; s++) {
+        if (students[s].graduated) {
+            console.log(students[s].studentName + ' was studying at in ' + students[s].university + ' university.');
         } else {
             console.log(students[s].studentName + ' is studying at in ' + students[s].university + ' university.');
         }
@@ -72,17 +77,20 @@ function studentsAndUnis(students){
 
 //Task 5.
 function avgUniScore(students) {
-    let sumScore = 0;
-    for(let u = 0; u < students.length; u++){
-        if(students[0].university === students[u].university){
-            sumScore += students[u].finalScore;
-            // console.log(sumScore);
-        } else {
-        sumScore = students[u].finalScore;
-        console.log(students[u].university + ' AVG score is: ' + sumScore);
+    let unis = [];
+    for (let v = 0; v < students.length; v++) {
+        if (unis.indexOf(students[v].university) < 0) {
+            unis.push(students[v].university);
         }
     }
 
+    for (let n = 0; n < unis.length; n++) {
+        let scores = students.filter(x => x.university === unis[n] && x.finalScore != 0).map(s => s.finalScore);
+        if (scores.length != 0) {
+            let avgScores = scores.reduce((a, b) => a + b) / scores.length;
+            console.log(unis[n] + ' : ' + avgScores);
+        }
+    }
 }
 
 solve();
